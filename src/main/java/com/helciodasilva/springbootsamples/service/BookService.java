5be203ca10c73d05e02a56ff4b476d72c6056901
Helcio da Silva <helciodasilva@gmail.com>
2019-01-26 19:33:58 -0200

    Adding Cache sample

--- src/main/java/com/helciodasilva/springbootsamples/service/BookService.java
+++ src/main/java/com/helciodasilva/springbootsamples/service/BookService.java
@@ -3,6 +3,8 @@ package com.helciodasilva.springbootsamples.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.helciodasilva.springbootsamples.dto.BookDTO;
@@ -18,15 +20,24 @@ public class BookService {
 
   @Autowired
   private BookRepository repository;
-
-  public BookEntity retrieveById(Long id) {
-    log.debug("Execute retrieve by id={}", id);
-    Optional<BookEntity> book = repository.findById(id);
+  
+  @Cacheable(value = "book", key = "#userId", condition = "#userId != null")
+  public BookEntity retrieveById(Long userId) {
+    log.debug("Execute retrieve by id={}", userId);
+    Optional<BookEntity> book = repository.findById(userId);
 
     if (!book.isPresent()) {
-      throw new ResourceNotFoundException(id, "user not found");
+      throw new ResourceNotFoundException(userId, "user not found");
     }
     return book.get();
+  }
+  
+  @CacheEvict(value = "book", key = "#userId")
+  public void resetOnSurname(String userId) {
+  }
+
+  @CacheEvict(value = "book", allEntries = true)
+  public void resetAllEntries() {
   }
 
   public void create(BookDTO dto) {
